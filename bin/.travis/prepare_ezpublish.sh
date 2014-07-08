8c27cf00943760e91f86a69fa28fd765a5e2882b
MLoureiro <mloureiro.dev@gmail.com>
2014-07-08 17:02:39 +0100

    New test set

--- bin/.travis/prepare_ezpublish.sh
+++ bin/.travis/prepare_ezpublish.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Script to prepare eZPublish installation
 
@@ -6,10 +6,10 @@ echo "> Install dependencies through composer"
 composer install --dev --prefer-dist
 
 echo "> Set folder permissions"
-sudo setfacl -R -m u:www-data:rwx -m u:www-data:rwx \
-     ezpublish/{cache,logs,config,sessions} ezpublish_legacy/{design,extension,settings,var} web
-sudo setfacl -dR -m u:www-data:rwx -m u:`whoami`:rwx \
-     ezpublish/{cache,logs,config,sessions} ezpublish_legacy/{design,extension,settings,var} web
+sudo chown -R www-data:www-data ezpublish/{cache,logs,config,sessions} \
+    ezpublish_legacy/{design,extension,settings,var} web
+sudo chmod -R og+rwX ezpublish/{cache,logs,config,sessions} \
+    ezpublish_legacy/{design,extension,settings,var} web
 
 echo "> Run assetic dump for behat env"
 php ezpublish/console --env=behat assetic:dump
