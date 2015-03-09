d7a457d545588f630d8f0f932f2c8516731094a2
André R <ar@ez.no>
2015-03-09 17:10:52 +0100

    Revert "EZP-23949: enabled and configured PlatformInstallerBundle"
    This reverts commit a62c3c6ea1e6f9196365039e5e914f037449325d.
    
    NB: Keeps PlatformInstallerBundle enabled, as it is requrired by current versions of DemoBundle

--- bin/.travis/prepare_ezpublish.sh
+++ bin/.travis/prepare_ezpublish.sh
@@ -9,8 +9,8 @@ echo "> Install dependencies through composer"
 composer install -n --prefer-dist
 
 echo "> Set folder permissions"
-sudo find {ezpublish/{cache,logs,config,sessions},web} -type d | sudo xargs chmod -R 777
-sudo find {ezpublish/{cache,logs,config,sessions},web} -type f | sudo xargs chmod -R 666
+sudo find {ezpublish/{cache,logs,config,sessions},ezpublish_legacy/{design,extension,settings,var},web} -type d | sudo xargs chmod -R 777
+sudo find {ezpublish/{cache,logs,config,sessions},ezpublish_legacy/{design,extension,settings,var},web} -type f | sudo xargs chmod -R 666
 
 echo "> Run assetic dump for behat env"
 php ezpublish/console --env=behat assetic:dump
